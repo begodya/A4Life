5ab37e86a21f52a420bf147e25dd8e262c9101ba
hmily-xinxiang@163.com <hmily-xinxiang@163.com>
2016-04-05 17:27:23 +0800

    A4Life-iOS

---
+++ A4Life-iOS/A4Life-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A4Life-iOS
+//
+//  Created by Bei Wang on 4/5/16.
+//  Copyright © 2016 Begodya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
